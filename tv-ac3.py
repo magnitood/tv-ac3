b51adce0db889acedaeeb59295fe740eca5cfee3
Parthasarathi <theeparthasarathi@gmail.com>
2026-03-19 00:49:59 +0530

    added backtracking algo, need ac3

--- tv-ac3.py
+++ tv-ac3.py
@@ -12,6 +12,7 @@ yellow = 220
 brown = 137
 
 fg = lambda n: "\x1b[38:5:"+str(n)+"m"
+# fg = lambda n: "\x1b[0m"
 bg = lambda n: "\x1b[48:5:"+str(n)+"m"
 rst = "\x1b[0m"
 
@@ -26,36 +27,8 @@ color = {
     7: fg(yellow)+"7"+rst,
     8: fg(brown)+"8"+rst,
     9: fg(black)+"9"+rst,
+    None: " ",
 }
-
-def generate_random_solvable_board(rows, cols):
-    board = []
-    for r in range(0, rows):
-        row = []
-        for c in range(0, cols):
-            col = []
-            if (r == 0):
-                col.append(rand.randint(0, 9))
-            else:
-                col.append(board[r-1][c][2])
-            col.append(rand.randint(0, 9))
-            col.append(rand.randint(0, 9))
-            if (c == 0):
-                col.append(rand.randint(0, 9))
-            else:
-                col.append(row[c-1][1])
-            row.append(col)
-        board.append(row)
-    return board
-
-def board_to_domain(board):
-    cells = []
-    for r in range(0, rows):
-        for c in range(0, cols):
-            cells.append(board[r][c])
-
-    rand.shuffle(cells)
-    return cells
 
 def print_board(board, rows, cols):
     b = board
@@ -85,7 +58,116 @@ def print_board(board, rows, cols):
         print("+-------", end='')
     print("+")
 
-rows = 4
-cols = 4
-board = generate_random_solvable_board(rows, cols)
-print_board(board, rows, cols)
+
+def generate_random_solved_board(rows, cols):
+    board = []
+    for r in range(0, rows):
+        row = []
+        for c in range(0, cols):
+            col = []
+            if (r == 0):
+                col.append(rand.randint(0, 9))
+            else:
+                col.append(board[r-1][c][2])
+            col.append(rand.randint(0, 9))
+            col.append(rand.randint(0, 9))
+            if (c == 0):
+                col.append(rand.randint(0, 9))
+            else:
+                col.append(row[c-1][1])
+            row.append(col)
+        board.append(row)
+    return board
+
+def board_to_domain(board, rows, cols):
+    cells = []
+    for r in range(0, rows):
+        for c in range(0, cols):
+            cells.append(board[r][c])
+    rand.shuffle(cells)
+
+    domain = []
+    for r in range(0, rows):
+        row = []
+        for c in range(0, cols):
+            row.append(cells.copy())
+        domain.append(row)
+    return domain
+
+def generate_random_solvable_domain(rows, cols):
+    board = generate_random_solved_board(rows, cols)
+    return board_to_domain(board, rows, cols)
+
+def init_board(rows, cols):
+    board = []
+    for r in range(0, rows):
+        row = []
+        for c in range(0, cols): 
+            col = [None, None, None, None]
+            row.append(col)
+        board.append(row)
+    return board
+
+
+def is_board_full(board, rows, cols):
+    for r in range(0, rows):
+        for c in range(0, cols):
+            if (board[r][c][0] == None):
+                return False
+    return True
+
+def is_board_valid(board, rows, cols):
+    for r in range(0, rows):
+        for c in range(0, cols):
+            if (board[r][c][0] == None):
+                return False
+            if (r != 0):
+                if (board[r][c][0] != board[r-1][c][2]):
+                    return False
+            if (c != 0):
+                if (board[r][c][3] != board[r][c-1][1]):
+                    return False
+    return True
+
+def next_unassigned_var(board, rows, cols):
+    for r in range(0, rows):
+        for c in range(0, cols):
+            if (board[r][c][0] == None):
+                return r, c
+
+# AC-3
+# returns True and alters domain if arc-consistent
+# returns False
+def apply_inference(domain, board, rows, cols):
+    return True
+
+# board, rows and cols together is the assignement
+# domain is the csp
+
+count = 0
+def backtracking_solver(domain, board, rows, cols):
+    if (is_board_full(board, rows, cols)):
+        return True
+
+    r, c = next_unassigned_var(board, rows, cols)
+    for value in domain[r][c]:
+        board[r][c] = value
+        old_domain = domain.copy()
+        if apply_inference(domain, board, rows, cols):
+            result = backtracking_solver(domain, board, rows, cols)
+            if result:
+                return True
+            domain = old_domain
+        board[r][c] = [None, None, None, None]
+    return False
+
+rows = 5
+cols = 5
+domain = generate_random_solvable_domain(rows, cols)
+print(domain[0][0])
+board = init_board(rows, cols)
+result = backtracking_solver(domain, board, rows, cols)
+if result:
+    print_board(board, rows, cols)
+else:
+    print("No Solution", count)
